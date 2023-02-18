83f9dda87a5be3301f79acde6ce38ec35256bf40
kimyeonchul <yeonchul0807@nate.com>
2023-02-18 16:10:40 +0900

    Feat: Add Header Component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './Header.module.css';
+
+export default function Header({ user }) {
+  return (
+    <header>
+      <div className={styles.titleWrap}>
+        <img
+          className={styles.titleLogo}
+          src="images\titlelogo.png"
+          alt="error"
+        />
+        <span className={styles.titleName}>{user}</span>님 페이지입니다.
+      </div>
+    </header>
+  );
+}
