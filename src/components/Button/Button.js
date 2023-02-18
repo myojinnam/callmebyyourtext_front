5b19bed76d1c7821126a4ea8ecddf5fdbf742a61
kimyeonchul <yeonchul0807@nate.com>
2023-02-19 01:14:08 +0900

    Feat: button component add props 'background'

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import styles from './Button.module.css';
 
-export default function Button({ value, onClick, width, fontSize }) {
+export default function Button({
+  value,
+  onClick,
+  width,
+  fontSize,
+  background,
+}) {
   return (
     <button
       className={styles.button}
       onClick={onClick}
       value={value}
-      style={{ width: width, fontSize: fontSize }}
+      style={{ width: width, fontSize: fontSize, background: background }}
     >
       {value}
     </button>
