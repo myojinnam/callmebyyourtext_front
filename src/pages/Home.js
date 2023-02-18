5c41c843c33932c1ac6ca933c3df1d77df80b1a7
AsherOh <wnstj701@naver.com>
2023-02-18 15:28:37 +0900

    Style: landing page redirect & style success

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,39 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import { primaryColor, secondaryColor } from '../styles/GlobalStyle';
 
 const Title = styled.p`
-  font-family: NotoSansKRBlack;
+  font-family: 'Noto Sans KR Black';
+  font-style: italic;
+  font-size: 1.5rem;
+  color: ${primaryColor};
+  margin-bottom: 0;
 `;
+
+const Bold = styled.span`
+  font-family: 'Noto Sans KR Bold';
+  font-weight: 900;
+`
+
+const SubTitle = styled.p`
+  font-family: 'Noto Sans KR Bold';
+  font-size: 0.8rem;
+  font-weight: 900;
+  color: ${secondaryColor};
+  text-align: center;
+`
+
+const Bracket = styled.span`
+  visibility: hidden;
+`
 
 const Home = () => {
   const navigate = useNavigate();
   const loading = () => {
     setTimeout(() => {
       navigate('/question');
-    }, 500000);
+    }, 3000);
   };
 
   useEffect(() => {
@@ -23,7 +45,8 @@ const Home = () => {
 
   return (
     <>
-      <Title>Call me by your TEXT</Title>
+      <Title>Call me by your <Bold>TEXT</Bold></Title>
+      <SubTitle>나의 (<Bracket>빈칸</Bracket>), 당신의 ____</SubTitle>
     </>
   );
 };
