3b9b87d953b04eeedec7cfd0c492bec2135cacda
kimyeonchul <yeonchul0807@nate.com>
2023-02-18 18:31:42 +0900

    Feat : add button's prop fontSize

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import styles from './Button.module.css';
 
-export default function Button({ value, onClick, width }) {
+export default function Button({ value, onClick, width, fontSize }) {
   return (
-    <button onClick={onClick} value={value} style={{ width: width }}>
+    <button
+      className={styles.button}
+      onClick={onClick}
+      value={value}
+      style={{ width: width, fontSize: fontSize }}
+    >
       {value}
     </button>
   );
