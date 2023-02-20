1a2360fc584ce317d100aff820aa0c8826b7119d
yoona1110 <gjdbsdk18@icloud.com>
2023-02-21 03:13:02 +0900

    Design: Add PointCheck UI

--- src/components/PointCheck/PointCheck.js
+++ src/components/PointCheck/PointCheck.js
@@ -1,9 +1,33 @@
 import React, { Component } from "react";
 import styles from "./pointCheck.module.css";
+import Back from "../../assets/images/back.png";
+import Menu from "../../assets/images/menu.png";
+import Notification from "../../assets/images/notification.png";
 
 class pointCheck extends Component {
   render() {
-    return <div className={styles.main}></div>;
+    const username = "홍길동";
+    const point = 500;
+    return (
+    <div className={styles.main}>
+      <img src={Back} alt="error"/>
+      <p className={styles.topBtn}>
+        <img src={Menu} alt="error"/>
+        <img src={Notification} alt="error"/>
+      </p>
+      <p className={styles.pointBox}> 
+        <text className={styles.pointFont}
+          style={{fontSize: '15px',
+            float: 'right'}}>포인트 : {point}</text>
+        <text className={styles.pointFont}
+          style={{fontSize: '25px'}}>{username}</text>
+      </p>
+      <p className={styles.notiWordBundle}>
+        <text className={styles.notiWord}>• 포인트는 댓글 열람 시 사용 가능합니다.</text>
+        <text className={styles.notiWord}>• 포인트는 다른 유저 게시물에 댓글을 달아 얻을 수 있습니다.</text>
+      </p>
+    </div>
+    );
   }
 }
 
