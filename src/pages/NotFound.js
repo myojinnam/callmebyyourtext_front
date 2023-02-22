7c5fb3a65fc98e94b75314e63bc7f4e935cca688
AsherOh <102159721+AsherOh@users.noreply.github.com>
2023-02-23 01:34:20 +0900

    Feat: Link home page from NotFound page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -2,8 +2,14 @@ import React from "react";
 import { Img, SubTitle, Wrapper } from "../components/Styled";
 import Title from "../components/Title/Title";
 import Logo from "../assets/images/loadingLogo.png";
+import PrimaryBtn from "../components/Button/PrimaryBtn";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
+  const navigate = useNavigate();
+  const redirectURL = () => {
+    navigate("/", { replace: true });
+  };
   return (
     <>
       <Wrapper>
@@ -14,6 +20,7 @@ const NotFound = () => {
           <br />
           다시 시도해주세요.
         </SubTitle>
+        <PrimaryBtn btnName={"돌아가기"} onClick={redirectURL}></PrimaryBtn>
       </Wrapper>
     </>
   );
