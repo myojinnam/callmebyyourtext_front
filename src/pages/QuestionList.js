6a152350812e95db00ab850287e19f860839f32b
AsherOh <102159721+AsherOh@users.noreply.github.com>
2023-02-22 20:27:07 +0900

    Test: Test createQuestion page using axios

--- src/pages/QuestionList.js
+++ src/pages/QuestionList.js
@@ -5,40 +5,38 @@ import TitleLogo from "../assets/images/titleLogo.png";
 import { SmallImg, Wrapper } from "../components/Styled";
 import { Box, Typography } from "@mui/material";
 import { pointColor, primaryColor } from "../styles/GlobalStyle";
+import ListBtn from "../components/Button/ListBtn";
 
 const QuestionList = () => {
   const navigate = useNavigate();
   const userName = localStorage.getItem("name");
   const { isLoggedIn } = useContext(AuthContext);
-  if (isLoggedIn) {
-    return (
-      <>
-        <Wrapper>
-          <Typography
-            variant="h5"
-            sx={{
-              color: `${pointColor}`,
-              borderBottom: `1px solid ${primaryColor}`,
-              marginBottom: "30%",
-            }}
-          >
-            <SmallImg src={TitleLogo} /> {userName}님의 질문 리스트
-          </Typography>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          ></Box>
-        </Wrapper>
-      </>
-    );
-  } else {
-    alert("로그인 후 이용해 주세요.");
-    navigate("/");
-    return;
-  }
+
+  return (
+    <>
+      <Wrapper>
+        <Typography
+          variant="h5"
+          sx={{
+            color: `${pointColor}`,
+            borderBottom: `1px solid ${primaryColor}`,
+            marginBottom: "10%",
+          }}
+        >
+          <SmallImg src={TitleLogo} /> {userName}님의 질문 리스트
+        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <ListBtn btnName={"test"}></ListBtn>
+        </Box>
+      </Wrapper>
+    </>
+  );
 };
 
 export default QuestionList;
